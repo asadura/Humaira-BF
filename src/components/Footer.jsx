27a8cbdc2c1138a5b2022a4c898085504af34aa3
asadura <mianasadullahzubair@gmail.com>
2025-10-18 04:30:35 -0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,9 +13,9 @@ export default function Footer() {
           <h2 className="text-yellow-400 font-bold text-lg mb-4">Contact</h2>
           <ul className="space-y-2 text-gray-100 text-sm">
             <li>Malbern, Australia</li>
-            <li>HDF.vitswd@vit.edu</li>
+            <li>admin@hdfintl.com</li>
             <li>+61 431 457 457</li>
-            <li>+61 411 570 985</li>
+            <li>+92 305 463 8070</li>
             
           </ul>
         </div>
